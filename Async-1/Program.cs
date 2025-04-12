dd2a090394232ce1c46f574b36bc9b5f93313b85
Datfreakk <praveenreddy596@gmail.com>
2025-04-12 17:31:15 +0100

    .

---
+++ Async-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Coding_2025_practise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aync!");
+        }
+    }
+}
