60b4ea1bdbdafee3defb4abfb20e40f687067c02
Praveen Reddy <PraveenR@cubictelecom.com>
2025-04-14 23:07:40 +0100

    First async code

---
+++ Async_one/Async_one.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Coding_2025_practise
+{
+    class Async_one
+    {
+        static async  Task Main(string[] args)
+        {
+            await Takesinteger(3000);
+        }
+
+        public static async Task Takesinteger(int milliseconds)
+        {
+            Console.WriteLine($"Starting async method with {milliseconds}");
+            await Task.Delay(milliseconds);
+            Console.WriteLine("Finishing  async method");
+        }
+    }
+}
