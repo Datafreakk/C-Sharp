e97b29502dce0cf0292c33013c1e0865d723134f
Praveen Reddy <PraveenR@cubictelecom.com>
2025-04-28 20:07:59 +0100

    cancellationtoken code code added

--- Async_6/Program.cs
+++ Async_6/Program.cs
@@ -1,9 +1,54 @@
-﻿namespace Async_6;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
-class Program
+namespace Async_5;
+
+public class Program
 {
-    static void Main(string[] args)
+    public async Task longrunningtask(CancellationToken cancellationToken, TimeSpan timeout)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Long running taks started");
+        using (var timeoutCts = new CancellationTokenSource())
+        {
+            using (var linktimeoutwithusercancellation =
+                   CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, timeoutCts.Token))
+            {
+                timeoutCts.CancelAfter(timeout);
+                try
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        linktimeoutwithusercancellation.Token.ThrowIfCancellationRequested();
+                        await Task.Delay(1000, linktimeoutwithusercancellation.Token);
+                        Console.WriteLine($"Processing{i + 1}seconds passed");
+                    }
+                    Console.WriteLine("Operation completed successfully.");
+                }
+                catch (OperationCanceledException)
+                {
+                    if (timeoutCts.Token.IsCancellationRequested)
+                    {
+                        Console.WriteLine("Operation ios cancelled by timeout");
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Operation ios cancelled by user");
+                    }
+
+                    throw;
+
+                }
+            }
+        }
     }
+
+    public static async Task Run()
+    {
+        
+    }
+    
+    
 }
