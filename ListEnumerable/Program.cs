21f2accf82a3358d9a9637e75001d96d94c609de
Praveen Reddy <PraveenR@cubictelecom.com>
2025-05-30 09:48:23 +0100

    update lsist enumerable

---
+++ ListEnumerable/Program.cs
@@ -0,0 +1,51 @@
+﻿using static ListEnumerable.Program;
+using System.Collections.Generic;
+
+namespace ListEnumerable
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+  
+            //List<Products> productss = GetProductss();
+            IEnumerable<Products> products = GetProducts();
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Name: {product.Name}, Price: {product.Price}");
+            }
+        }
+
+        public static IEnumerable<Products> GetProducts()
+        {
+            return  new List<Products>
+            {
+                new Products { Name = "iPhone", Price = 10 },
+                new Products { Name = "Samsung", Price = 20 },
+                new Products { Name = "OnePlus", Price = 30 }
+            };
+
+        }
+        //We can declare like this but Ienumerable is better for iteration 
+        public static List<Products> GetProductss()
+        {
+            return new List<Products>
+            {
+                new Products { Name = "iPhone", Price = 10 },
+                new Products { Name = "Samsung", Price = 20 },
+                new Products { Name = "OnePlus", Price = 30 }
+            };
+        }
+
+        public class Products
+        {
+            public string Name { get; set; }
+            public int Price { get; set; }
+        }
+
+
+
+    }
+}
+
