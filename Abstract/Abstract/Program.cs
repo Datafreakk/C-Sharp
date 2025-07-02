878f333fba8d1a94f4f27bccf74daa72f4c3525c
Praveen Reddy <PraveenR@cubictelecom.com>
2025-07-02 15:13:08 +0100

    .

---
+++ Abstract/Abstract/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Abstract;
+
+public abstract class Vehicle
+{
+    public string  Name { get; set; }
+
+    public Vehicle(string name)
+    {
+        Name = name;
+    }
+
+    public abstract void startEngine();
+
+    public virtual void Drive()
+    {
+        Console.WriteLine($"Abstract Vehicle Drives");
+    }
+}
+
+public class Car : Vehicle
+{
+    public Car(string name) : base(name)
+    {
+    }
+
+    public override void startEngine()
+    {
+        Console.WriteLine($"Starting Engine of the car {Name}");
+    }
+
+    public override void Drive()
+    {
+        Console.WriteLine($"Driving from car {Name}");
+    }
+}
+
+// public class Program
+// {
+//     public static void Main()
+//     {
+//         Car car = new Car("Bmw");
+//         car.startEngine();
+//         car.Drive();
+//     }
+// }
+
