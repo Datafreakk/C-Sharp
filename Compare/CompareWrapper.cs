c7b2e316392f680785d3a1fb284416649624d4b5
Praveen Reddy <PraveenR@cubictelecom.com>
2025-09-10 16:08:50 +0100

    Added different compare methods

---
+++ Compare/CompareWrapper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CompareWrapper<T> where T : IComparable<T>
+{
+    public T Value { get; set; }
+
+    public CompareWrapper(T value)
+    {
+        Value = value;
+    }
+
+    // Custom comparison method
+    public int CompareValues(CompareWrapper<T> other)
+    {
+        if (other == null) return 1;
+        return Value.CompareTo(other.Value);
+    }
+
+    public override string ToString() => Value?.ToString() ?? "null";
+}
+//
+// public class Program
+// {
+//     public static void Main()
+//     {
+//         var list = new List<CompareWrapper<int>>()
+//         {
+//             new CompareWrapper<int>(20),
+//             new CompareWrapper<int>(5),
+//             new CompareWrapper<int>(15),
+//             new CompareWrapper<int>(10)
+//         };
+//
+//         Console.WriteLine("Before sorting:");
+//         foreach (var item in list)
+//         {
+//             Console.WriteLine(item);
+//         }
+//
+//         // Using custom comparison method in a lambda for Sort
+//         list.Sort((x, y) => x.CompareValues(y));
+//
+//         Console.WriteLine("\nAfter sorting:");
+//         foreach (var item in list)
+//         {
+//             Console.WriteLine(item);
+//         }
+//     }
+// }
---
+++ Compare/comparerecord.cs
@@ -0,0 +1,43 @@
+﻿namespace Compare;
+
+public record  CompareRecord(long Value ) : IComparable<CompareRecord>
+{
+    public static CompareRecord  Create()
+    {
+        return new CompareRecord(Guid.NewGuid().GetHashCode());
+    }
+
+    public int CompareTo(CompareRecord?  others)
+    {
+        if (others is null ) return 1;
+        return Value.CompareTo(others.Value);
+    }
+
+    public override string ToString()
+    {
+        return $"{Value: X}";
+    }
+}
+
+//usage 
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var list = new List<CompareRecord>
+        {
+            new CompareRecord(6),
+            new CompareRecord(9),
+            new CompareRecord(-1),
+
+        };
+
+        list.Sort();
+
+        foreach (var item in list )
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
